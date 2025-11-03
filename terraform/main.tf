71354ca8b2c8b5b56f0eb4574f6076b254209ee3
Osama-devv <osama.shad@technyxsystems.com>
2025-11-03 18:28:21 +0500

    Grafana

--- terraform/main.tf
+++ terraform/main.tf
@@ -137,6 +137,14 @@ resource "aws_security_group" "ecs_sg" {
     self      = true
   }
 
+  ingress {
+  from_port       = 3000
+  to_port         = 3000
+  protocol        = "tcp"
+  security_groups = [aws_security_group.alb_sg.id]
+  }
+
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -261,6 +269,30 @@ resource "aws_lb_target_group" "tg" {
   }
 }
 
+resource "aws_lb_target_group" "grafana" {
+  name        = "${local.name_prefix}-tg-grafana"
+  port        = 3000
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.this.id
+  target_type = "ip"
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    unhealthy_threshold = 2
+    healthy_threshold   = 2
+    matcher             = "200"
+    timeout             = 5
+  }
+
+  tags = { Name = "${local.name_prefix}-tg-grafana" }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 80
@@ -275,6 +307,23 @@ resource "aws_lb_listener" "http" {
     aws_lb_target_group.tg
   ]
 }
+
+resource "aws_lb_listener_rule" "grafana_rule" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.grafana.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/grafana/*"]
+    }
+  }
+}
+
 
 # -----------------------
 # CloudWatch Logs
@@ -476,6 +525,12 @@ resource "aws_ecs_service" "grafana" {
     security_groups  = [aws_security_group.ecs_sg.id]
     assign_public_ip = false
   }
+
+  load_balancer {
+  target_group_arn = aws_lb_target_group.grafana.arn
+  container_name   = "grafana"
+  container_port   = 3000
+}
 }
 
 # -----------------------
